b6234dce033434bf54ca25c22faecb44f9481d16
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-06-28 14:44:30 +0300

    Adjusting Structure

--- calculator.h
+++ calculator.h
@@ -1,9 +1,15 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
+
 #define MAX_SYMBOL_LENGTH 20
+
+
 struct SymbolTab {
     char name[MAX_SYMBOL_LENGTH];
     float value;
 };
+
 struct SymbolTab* symbolLookup(const char* symbol);
+
+
 #endif
